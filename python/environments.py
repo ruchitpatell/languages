2027e82e99ad0454304cf21359edbdd902109948
Ruchit Patel <ruchittpatell@gmail.com>
2024-07-31 13:09:20 -0700

    Virtual environments (#1)
    * Virtual Environments
    
    * Added commands
    
    ---------
    
    Co-authored-by: Ruchit Patel <rpatel18@ucsc.edu>

---
+++ python/environments.py
@@ -0,0 +1,14 @@
+"""
+Virtual Environments
+
+    - Docker for python projects
+    - All the required libraries and python version in that environment
+    - Create activate cd deactive
+
+    1. Make environment     (python3 -m venv envname)
+    2. Start                (source env/bin/activate)
+    3. Install requirements (pip install and pip freeze > requirements.txt)
+    4. Once done, delete venv to avoid bloat (rm -rf venv)
+
+Other: Jupyter, Conda
+"""
