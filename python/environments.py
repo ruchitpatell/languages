38f84411f2a6091c173e767c4754007c08a81a08
Ruchit Patel <rpatel18@ucsc.edu>
2024-07-31 13:06:24 -0700

    Virtual Environments

---
+++ python/environments.py
@@ -0,0 +1,14 @@
+"""
+Virtual Environments
+
+    - Docker for python projects
+    - All the required libraries and python version in that environment
+    - Create activate cd deactive
+
+    1. Make environment
+    2. Start
+    3. Install requirements
+    4. Once done, delete venv to avoid bloat
+
+Other: Jupyter, Conda
+"""
