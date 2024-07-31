b14e3fb3cd55b43b099a60547b4f0aa9a42d30ee
Ruchit Patel <rpatel18@ucsc.edu>
2024-07-31 13:08:29 -0700

    Added commands

--- python/environments.py
+++ python/environments.py
@@ -5,10 +5,10 @@ Virtual Environments
     - All the required libraries and python version in that environment
     - Create activate cd deactive
 
-    1. Make environment
-    2. Start
-    3. Install requirements
-    4. Once done, delete venv to avoid bloat
+    1. Make environment     (python3 -m venv envname)
+    2. Start                (source env/bin/activate)
+    3. Install requirements (pip install and pip freeze > requirements.txt)
+    4. Once done, delete venv to avoid bloat (rm -rf venv)
 
 Other: Jupyter, Conda
 """
