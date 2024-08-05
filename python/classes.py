2a01c1b360c3d16c1bd63bee841d6a42281c9e72
Ruchit Patel <ruchittpatell@gmail.com>
2024-08-04 22:28:30 -0700

    classes: better examples

--- python/classes.py
+++ python/classes.py
@@ -19,90 +19,98 @@ my_object.method1()
 
 # Inheritance 
 # Inherits attributes and methods
-class ParentClass:
-    def __init__(self, parent_attribute):
-        self.parent_attribute = parent_attribute
+class Vehicle:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
 
-    def parent_method(self):
-        return self.parent_attribute
+    def display_info(self):
+        return f"{self.year} {self.make} {self.model}"
 
-class ChildClass(ParentClass):
-    def __init__(self, parent_attribute, child_attribute):
-        super().__init__(parent_attribute)
-        self.child_attribute = child_attribute
+class Car(Vehicle):
+    def __init__(self, make, model, year, num_doors):
+        super().__init__(make, model, year)
+        self.num_doors = num_doors
 
-    def child_method(self):
-        return self.child_attribute
+    def display_info(self):
+        return f"{super().display_info()}, {self.num_doors}-door"
 
-child_object = ChildClass("parent_value", "child_value")
-child_object.parent_method()
-child_object.child_method()
+car = Car("Toyota", "Camry", 2020, 4)
+car.display_info() # Overrides super().display_info()
 
 
 # Multiple Inheritance
-class Base1:
-    def method_base1(self):
-        return "Base1"
+class Device:
+    def power_on(self):
+        return "Device powered on"
 
-class Base2:
-    def method_base2(self):
-        return "Base2"
+class Charger:
+    def charge(self):
+        return "Charging device"
 
-class Derived(Base1, Base2):
-    def method_derived(self):
-        return "Derived"
+class Phone(Device, Charger):
+    def make_call(self):
+        return "Making a call"
 
-derived = Derived()
-derived.method_base1()
-derived.method_base2()
-derived.method_derived()
+phone = Phone()
+phone.power_on()
+phone.charge()
+phone.make_call()
 
 
 # Polymorphism
 # Allows methods to do different things based on the object it is acting upon
-class Animal:
-    def sound(self):
+class Payment:
+    def process_payment(self, amount):
         raise NotImplementedError("Subclass must implement abstract method")
 
-class Dog(Animal):
-    def sound(self):
-        return "Bark"
+class CreditCardPayment(Payment):
+    def process_payment(self, amount):
+        return f"Processing credit card payment of {amount}"
 
-class Cat(Animal):
-    def sound(self):
-        return "Meow"
+class PayPalPayment(Payment):
+    def process_payment(self, amount):
+        return f"Processing PayPal payment of {amount}"
 
-def make_sound(animal):
-    return animal.sound()
+def process(payment, amount):
+    return payment.process_payment(amount)
 
-dog = Dog()
-cat = Cat()
-make_sound(dog) # Bark
-make_sound(cat) # Meow
+credit_card_payment = CreditCardPayment()
+paypal_payment = PayPalPayment()
+
+process(credit_card_payment, 100)
+process(paypal_payment, 200)    # uses paypal payment
 
 
 # Abstraction
 # Provide only essential information and hiding the implementation details
 from abc import ABC, abstractmethod
 
-class Animal(ABC):
+class Storage(ABC):
     @abstractmethod
-    def sound(self):
+    def save(self, data):
         pass
 
-class Dog(Animal):
-    def sound(self):
-        return "Bark"
+    @abstractmethod
+    def load(self):
+        pass
 
-class Cat(Animal):
-    def sound(self):
-        return "Meow"
+class FileStorage(Storage):
+    def save(self, data):
+        # with open("data.txt", "w") as file:
+        #     # file.write(data)
+        #     pass
+        return "Data saved to file"
 
-dog = Dog()
-cat = Cat()
+    def load(self):
+        # with open("data.txt", "r") as file:
+        #     pass
+        return
 
-dog.sound()
-cat.sound()
+storage = FileStorage()
+storage.save("Example data")
+storage.load()
 
 
 # Data classes
@@ -121,52 +129,73 @@ x, y = point.x, point.y
 # Class Method and Static Methods
 # Class methods are methods that are bound to the class and not the instance of the class
 # Static methods do not have access to the class or instance
-class MyClass:
-    class_attribute = "class attribute"
+class Date:
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
 
     @classmethod
-    def class_method(cls):
-        return cls.class_attribute
+    def from_string(cls, date_string):
+        year, month, day = map(int, date_string.split('-'))
+        return cls(year, month, day)
 
     @staticmethod
-    def static_method():
-        return "static method"
+    def is_valid_date(date_string):
+        year, month, day = map(int, date_string.split('-'))
+        return 1 <= month <= 12 and 1 <= day <= 31
 
-MyClass.class_method()
-MyClass.static_method()
+date = Date.from_string("2024-08-04")
+# date.year, date.month, date.day
+Date.is_valid_date("2024-08-04")
 
 
 # Property Decorators
-class DecoratedClass:
-    def __init__(self, value):
-        self._value = value
+class Temperature:
+    def __init__(self, celsius):
+        self._celsius = celsius
 
     @property
-    def value(self):
-        return self._value
+    def celsius(self):
+        return self._celsius
 
-    @value.setter
-    def value(self, new_value):
-        self._value = new_value
+    @celsius.setter
+    def celsius(self, value):
+        self._celsius = value
 
-obj = DecoratedClass(10)
-obj.value = 20      # @value.setter gets called
+    @property
+    def fahrenheit(self):
+        return self._celsius * 9/5 + 32
+
+temp = Temperature(25)
+# temp.celsius) calls celsius(self
+# temp.fahrenheit) calls fahrenheit(self
+temp.celsius = 30   # calls @celsius.setter
 
 
 # Encapsulation
-class EncapsulationClass:
-    def __init__(self, value):
-        self.__value = value  # private variable
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance
 
-    def get_value(self):
-        return self.__value
+    def deposit(self, amount):
+        self.__balance += amount
+        return self.__balance
 
-    def set_value(self, new_value):
-        self.__value = new_value
+    def withdraw(self, amount):
+        if amount > self.__balance:
+            return "Insufficient funds"
+        self.__balance -= amount
+        return self.__balance
 
-obj = EncapsulationClass(10)
-obj.get_value()
-obj.set_value(20)
+    def get_balance(self):
+        return self.__balance
+
+account = BankAccount("Bob", 1000)
+account.deposit(500)
+account.withdraw(200)
+account.get_balance()
 
 
 # Dunder methods
@@ -178,29 +207,71 @@ class SpeicalClass:
         return f"MyClass with value {self.value}"
 
     def __add__(self, other):
-        return MyClass(self.value + other.value)
+        return SpeicalClass(self.value + other.value)
 
-obj1 = MyClass(10)
-obj2 = MyClass(20)
+obj1 = SpeicalClass(10)
+obj2 = SpeicalClass(20)
 obj3 = obj1 + obj2
 
 
+# Mixins (can't just use class methods or static methods?)
+class LogMixin:
+    def log(self, message):
+        return f"Log: {message}"
+
+class Application(LogMixin):
+    def run(self):
+        self.log("Application is running")
+
+app = Application()
+app.run()
+
+
+# Descriptors
+# Validating attributes (can just __set__ but cleaner code)
+class Positive:
+    def __init__(self, name):
+        self.name = name
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        if value < 0:
+            raise ValueError("Value must be positive")
+        instance.__dict__[self.name] = value
+
+class Account:
+    balance = Positive('balance')
+
+    def __init__(self, balance):
+        self.balance = balance
+
+account = Account(100)
+account.balance = 200
+# account.balance = -50  # This will raise a ValueError
+
+
 # Context Managers
-class MyContextManager:
+class DatabaseConnection:
     def __enter__(self):
-        # print("Entering the context")
-        return self
+        self.conn = self.connect_to_database()
+        return self.conn
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        # print("Exiting the context")
-        pass
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close_connection(self.conn)
 
-with MyContextManager():
-    # print("Inside the context")
+    def connect_to_database(self):
+        # print("Connecting to database")
+        return "Database Connection"
+
+    def close_connection(self, conn):
+        return "Closing database connection"
+
+with DatabaseConnection() as conn:
+    # Output:
+    # Connecting to database
+    # Database connection
+    # Closing database connection
     pass
-
-# Output:
-# Entering the context
-# Inside the context
-# Exiting the context
 
